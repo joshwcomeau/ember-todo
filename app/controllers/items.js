0b4dda81b7dcaada3a217e29481fdc03a0dd3f7f
Josh Comeau <jcomeau@digitalocean.com>
2019-06-15 19:49:15 -0400

    Whoops, fix it up

--- app/controllers/items.js
+++ app/controllers/items.js
@@ -4,7 +4,7 @@ export default Ember.Controller.extend({
   actions: {
     toggleItem(itemId) {
       this.get('store').findRecord('item', itemId).then(function(item) {
-        item.set('isCompleted', !item.isCompleted);
+        item.set('isCompleted', !item.get('isCompleted'));
         item.save();
       });
     }
